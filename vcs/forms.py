53657116ef90500f9285d6985e005064ce831788
Maryna Kostiuchenko <marina.k.kost@gmail.com>
2024-01-22 16:31:22 +0200

    form to create repository and repository list

--- vcs/forms.py
+++ vcs/forms.py
@@ -1,18 +1,13 @@
 from django import forms
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 from vcs.models import Repository
 
 
-class NewRepoForm(forms.Form):
+class NewRepoForm(forms.ModelForm):
     name = forms.CharField(max_length=255)
 
     class Meta:
         model = Repository
         fields = ['name']
-
-    # def save(self, commit=True):
-    #     repo = super(NewRepoForm, self).save(commit=False)
-    #     repo.email = self.cleaned_data['email']
-    #     if commit:
-    #         repo.save()
-    #     return repo
--- vcs/views.py
+++ vcs/views.py
@@ -1,18 +1,37 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import generic
+
 from .models import Repository
 from .forms import NewRepoForm
+import os
+from pathlib import Path
 
 
 def profileView(request):
-    return render(request, 'profile.html')
+    if request.user.is_authenticated:
+        return redirect('repositories')
+    else:
+        return redirect('login')
 
 
 def newRepo(request):
     if request.method == 'POST':
         form = NewRepoForm(request.POST)
         if form.is_valid():
-            # repo = form.save()  # FIXME Зберегти репозиторій
+            path = Path(__file__).resolve().parent.parent.parent
+            repositories_path = path.joinpath('Repositories', form.cleaned_data['name'], 'Content')
+            try:
+                os.makedirs(repositories_path)
+                os.mkdir(path.joinpath('Repositories', form.cleaned_data['name'], 'History'))
+            except FileExistsError:
+                print("Repository with the same name already exists")
+
+            detail = form.save(commit=False)
+            detail.author = request.user
+            detail.save()
+
             return redirect('profile')
 
     else:
@@ -20,8 +39,12 @@ def newRepo(request):
     return render(request, 'newrepo.html', {'form': form})
 
 
-class RepositoryListView(generic.ListView):
+class RepositoryListView(LoginRequiredMixin, generic.ListView):
     model = Repository
+    template_name = 'profile.html'
+
+    def get_queryset(self):
+        return Repository.objects.filter(author=self.request.user)
 
 
 class RepoDetailView(generic.DetailView):
